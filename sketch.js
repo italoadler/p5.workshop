afde59c4253461130ff09535efe5ad6124fb93a9
italoadler <adler.italo@gmail.com>
2023-08-29 19:32:35 -0300

    Jump sprite implementation 0.1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let spritesheet;
 let spritedata;
 let ground = [];
-let animation = [];
+let runAnimation = [];
+let jumpAnimation = [];
 let horse;
 let mountains;
 let hills;
@@ -16,26 +17,34 @@ let parallaxSpeedHill = 1;
 
 let groundX = 0;
 let parallaxSpeedGround = 8;
+let speedFlowers = parallaxSpeedGround;
 let flowers = [];
 let flowerClosedImg;
 let flowerOpenImg;
 
 function preload() {
-  mountains = loadImage('assets/background/scroll_bg_far.png');
-  hills = loadImage('assets/background/hills-scroll.png');
-  
+  mountains = loadImage("assets/background/scroll_bg_far.png");
+  hills = loadImage("assets/background/hills-scroll.png");
+
   for (let i = 1; i <= 3; i++) {
     const tileImg = loadImage(`assets/tile/grasses/tile_grass_0${i}.png`);
     ground.push(tileImg);
   }
 
-  galop = loadSound('assets/sound/galop.wav');
-  blossom = loadSound('assets/sound/tone.wav');
-  spritedata = loadJSON('horse.json');
-  spritesheet = loadImage('horse.png');
+  galop = loadSound("assets/sound/galop.wav");
+  blossom = loadSound("assets/sound/tone.wav");
+  spritedata = loadJSON("horse.json");
+  spritesheet = loadImage("horse.png");
 
-  flowerClosedImg = loadImage('assets/tile/flowers/flower.png');
-  flowerOpenImg = loadImage('assets/tile/flowers/flower-open.png');
+
+  // Preload the jump animations for the horse
+  for (let i = 0; i < 7; i++) {
+    let filename = `assets/sprite/horse/horse-jump-0${i}.png`;
+    jumpAnimation.push(loadImage(filename));
+  }
+
+  flowerClosedImg = loadImage("assets/tile/flowers/flower.png");
+  flowerOpenImg = loadImage("assets/tile/flowers/flower-open.png");
 }
 
 function setup() {
@@ -46,10 +55,10 @@ function setup() {
   for (let i = 0; i < frames.length; i++) {
     let pos = frames[i].position;
     let img = spritesheet.get(pos.x, pos.y, pos.w, pos.h);
-    animation.push(img);
+    runAnimation.push(img);
   }
 
-  horse = new Horse(animation, 0, 0, random(0.5, 0.4));
+  horse = new Horse(runAnimation, jumpAnimation, 0, 0, 0.3);
 
   for (let i = 0; i < 10; i++) {
     flowers.push(new Flower(flowerClosedImg));
@@ -58,16 +67,16 @@ function setup() {
 
 function draw() {
   background(mountains);
-  
+
   mountainX -= horse.speed * parallaxSpeedMountain;
   hillX -= horse.speed * parallaxSpeedHill;
-  
+
   image(mountains, mountainX, 0, width, height);
   image(mountains, mountainX + width, 0, width, height);
-  
+
   image(hills, hillX, height - hills.height - 71, width, hills.height);
   image(hills, hillX + width, height - hills.height - 71, width, hills.height);
-  
+
   groundX -= horse.speed * parallaxSpeedGround;
 
   let numberOfTiles = ceil(width / tileWidth);
@@ -91,10 +100,8 @@ function draw() {
     flower.show();
     flower.move();
 
-    if (horse.collides(flower) && flower.state === 'closed') {
-      blossom.play();
-      flower.state = 'shattered';
-      flower.shatter();
+    if (horse.collides(flower) && !flower.opened) {
+      flower.opened = true;
     }
   }
 }
@@ -114,80 +121,81 @@ function mousePressed() {
 }
 
 class Horse {
-  constructor(animation, x, y, speed) {
-    this.animation = animation;
-    this.speed = speed;
-    this.index = 0;
-    this.x = x;
-    this.y = y;
-    this.gravity = 0.8;
-    this.lift = -15;
-    this.velocity = 0;
-  }
-
-  show() {
-    let index = floor(this.index) % this.animation.length;
-    image(this.animation[index], this.x, this.y);
-  }
-
-  animate() {
-    this.index += this.speed;
-  }
-
-  jump() {
-    if (this.y == height - 200) {
-      this.velocity += this.lift;
-    }
-  }
-
-  applyGravity() {
-    this.velocity += this.gravity;
-    this.y += this.velocity;
-    if (this.y > height - 200) {
-      this.y = height - 200;
+    constructor(runAnimation, jumpAnimation, x, y, speed) {
+      this.runAnimation = runAnimation;
+      this.jumpAnimation = jumpAnimation;
+      this.speed = speed;
+      this.index = 0;
+      this.x = x;
+      this.y = y;
+      this.gravity = 0.5;
+      this.lift = -15;
       this.velocity = 0;
+      this.jumping = false; // Added to check if horse is currently jumping
+    }
+  
+    show() {
+      let index = floor(this.index) % this.runAnimation.length;
+      if(this.jumping) {
+        index = floor(this.index) % this.jumpAnimation.length;
+        image(this.jumpAnimation[index], this.x, this.y);
+    } else {
+        console.log("Current index:", index);
+        console.log("Current image:", this.runAnimation[index])
+        image(this.runAnimation[index], this.x, this.y);
+      }
+    }
+  
+    animate() {
+      this.index += this.speed;
+      
+    }
+  
+    jump() {
+      if (this.y == height - 200) {
+        this.velocity += this.lift;
+        this.jumping = true; // Set jumping to true when the horse jumps
+      }
+    }
+  
+    applyGravity() {
+      this.velocity += this.gravity;
+      this.y += this.velocity;
+      if (this.y > height - 200) {
+        this.y = height - 200;
+        this.velocity = 0;
+        this.jumping = false; // Reset jumping to false when the horse lands
+      }
+    }
+  
+    collides(flower) {
+      let horseWidth = this.runAnimation[0].width; // Use runAnimation here since it will have the horse's dimensions.
+      let horseHeight = this.runAnimation[0].height;
+      let flowerWidth = flower.image.width;
+      let flowerHeight = flower.image.height;
+  
+      return !(this.x + horseWidth < flower.x ||
+               this.x > flower.x + flowerWidth ||
+               this.y + horseHeight < flower.y ||
+               this.y > flower.y + flowerHeight);
     }
   }
-
-  collides(flower) {
-    let horseWidth = this.animation[0].width;
-    let horseHeight = this.animation[0].height;
-    let flowerWidth = flower.image.width;
-    let flowerHeight = flower.image.height;
-
-    return !(this.x + horseWidth < flower.x ||
-             this.x > flower.x + flowerWidth ||
-             this.y + horseHeight < flower.y ||
-             this.y > flower.y + flowerHeight);
-  }
-}
+  
 
 class Flower {
   constructor(img) {
     this.x = random(width, width * 2);
     this.y = height - img.height - 65;
     this.image = img;
-    this.speed = parallaxSpeedGround;
-    this.state = 'closed';
-    this.particles = [];
+    this.speed = parallaxSpeedGround; // Speed synced with the ground
+    this.opened = false;
   }
 
   show() {
-    if (this.state === 'closed') {
-      image(this.image, this.x, this.y);
-    } else if (this.state === 'opened') {
+    if (this.opened) {
       image(flowerOpenImg, this.x, this.y);
-    } else if (this.state === 'shattered') {
-      for (let p of this.particles) {
-        p.show();
-      }
-    }
-  }
-
-  shatter() {
-    for (let i = 0; i < 5; i++) {
-      const p = new Particle(this.x, this.y, this.image);
-      this.particles.push(p);
+    } else {
+      image(this.image, this.x, this.y);
     }
   }
 
@@ -195,35 +203,7 @@ class Flower {
     this.x -= this.speed;
     if (this.x < -this.image.width) {
       this.x = random(width, width * 2);
-      this.state = 'closed';
-      this.particles = [];
+      this.opened = false; // Reset the flower's state when it goes out of view
     }
-
-    for (let p of this.particles) {
-      p.update();
-    }
-  }
-}
-
-class Particle {
-  constructor(x, y, img) {
-    this.x = x;
-    this.y = y;
-    this.size = random(5, 15);
-    this.speedX = random(-2, 2);
-    this.speedY = random(-5, -1);
-    this.alpha = 255;
-    this.img = img;
-  }
-
-  update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-    this.alpha -= 5;
-  }
-
-  show() {
-    tint(255, this.alpha);
-    image(this.img, this.x, this.y, this.size, this.size);
   }
 }
